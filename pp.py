2444933700f60cf299a59057e5f014eb13091ee9
clinBox <132152727+clinBox@users.noreply.github.com>
2023-05-20 18:37:55 +0300

    alpha 1.1

--- pp.py
+++ pp.py
@@ -13,25 +13,41 @@ class GameSprite(sprite.Sprite):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
 class Player(GameSprite):
-    def update(self):
+    def update1(self):
         keys = key.get_pressed()
-        if keys[K_LEFT] and self.rect.x > 5:
-            self.rect.x -= self.speed 
-        if keys[K_RIGHT] and self.rect.x < 620:
-            self.rect.x += self.speed
-    def fire(self):
-        bullet = Bullet('bullet.png', self.rect.centerx, self.rect.top, 100, 15, 20)
-        bullets.add(bullet)
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed 
+        if keys[K_DOWN] and self.rect.y < 400:
+            self.rect.y += self.speed
+    def update2(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed 
+        if keys[K_s] and self.rect.y < 400:
+            self.rect.y += self.speed
+
 
 window = display.set_mode((700, 500))
 display.set_caption('Ping-pong')
-backgraund = transform.scale(image.load('image_bg.jpg'), (700,500))
+backgraund = transform.scale(image.load('2.jpg'), (700,500))
+
+speed_x = 3
+speed_y = 3
+
+
+rocket2 = Player('1.png',30,30,4, 20, 100)
+rocket1 = Player('1.png',650, 30, 4, 20, 100 )
+ball = GameSprite('5.jpg', 200, 200, 4, 65, 50)
+
 
 FPS = 60
 clock = time.Clock()
 
+font.init()
+font = font.SysFont('Arial', 80)
 
-
+lose1 = font.render('Player 1 lose', True, (255,0,0))
+lose2 = font.render('Player 2 lose', True, (255,0,0))
 
 run = True
 finish = False
@@ -41,5 +57,28 @@ while run:
             run = False
     if finish != True:
         window.blit(backgraund,(0, 0))
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        if ball.rect.y  <0 or ball.rect.y  >450:
+            speed_y *=- 1
+        if sprite.collide_rect(rocket1,ball) or sprite.collide_rect(rocket2,ball):
+            speed_x *=- 1
+        if ball.rect.x <0:
+            finish = True
+            window.blit(lose1,(200,200))
+        if ball.rect.x > 700:
+            finish = True
+            window.blit(lose2,(200,200))
+
+        rocket1.update1()
+        rocket2.update2()
+        rocket1.reset()
+        rocket2.reset()
+        ball.reset()
+       
+
+
+
+
     display.update()
     clock.tick(FPS)
